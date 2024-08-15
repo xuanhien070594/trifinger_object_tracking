ada07a1b627d5163adbc33541d40e5c3af1d0998
Hien Bui <xuanhien070594@gmail.com>
2024-08-15 15:22:52 -0400

    Minor change

--- scripts/cube_pose_estimation.py
+++ scripts/cube_pose_estimation.py
@@ -92,6 +92,9 @@ def run_cube_pose_tracker():
 
         if args.live_viewer:
             cv2.imshow(" | ".join(camera_names), stacked_image)
+            # stop if either "q" or ESC is pressed
+            if cv2.waitKey(1) in [ord("q"), 27]:  # 27 = ESC
+                break
 
         elapsed_time = time.perf_counter() - start_time
         remaining_time = (1.0 / args.publish_rate) - elapsed_time
@@ -103,6 +106,9 @@ def run_cube_pose_tracker():
             stacked_image,
             int(time.perf_counter() * 1e6),
         )
+
+    if not args.multi_process:
+        camera_backend.shutdown()
 
 
 if __name__ == "__main__":
